d95ef1272e62bb985ae1683e28a88cda1f5eda85
Tim Shilov <tim@affluent.io>
2023-09-15 12:26:00 +0100

    feat: added return statement parsing

--- ast/ast.go
+++ ast/ast.go
@@ -50,3 +50,15 @@ func (i *Identifier) expressNode() {}
 func (i *Identifier) TokenLiteral() string {
     return i.Token.Literal
 }
+
+type ReturnStatement struct {
+    Token       token.Token // the 'return' token
+    ReturnValue Expression
+}
+
+func (rs *ReturnStatement) statementNode() {
+
+}
+func (rs *ReturnStatement) TokenLiteral() string {
+    return rs.Token.Literal
+}
