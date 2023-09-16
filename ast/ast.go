38659694904a3bc3106e37d30cc0066af2c050a5
Tim Shilov <tim@affluent.io>
2023-09-16 21:18:13 +0100

    feat: added prefix operator parsing

--- ast/ast.go
+++ ast/ast.go
@@ -21,6 +21,27 @@ type Expression interface {
     expressionNode()
 }
 
+type PrefixExpression struct {
+    Token    token.Token // The prefix token, e.g. !
+    Operator string
+    Right    Expression
+}
+
+func (pe *PrefixExpression) expressionNode() {}
+func (pe *PrefixExpression) TokenLiteral() string {
+    return pe.Token.Literal
+}
+func (pe *PrefixExpression) String() string {
+    var out bytes.Buffer
+
+    out.WriteString("(")
+    out.WriteString(pe.Operator)
+    out.WriteString(pe.Right.String())
+    out.WriteString(")")
+
+    return out.String()
+}
+
 type Program struct {
     Statements []Statement
 }
