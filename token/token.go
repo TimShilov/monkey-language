911f1d3f0237174a794c92277a1cd12f160fd9fe
Tim Shilov <tim@shilov.dev>
2023-10-06 21:11:31 +0100

    feat: added support for string literals

--- token/token.go
+++ token/token.go
@@ -3,64 +3,65 @@ package token
 type TokenType string
 
 type Token struct {
-	Type    TokenType
-	Literal string
+    Type    TokenType
+    Literal string
 }
 
 const (
-	ILLEGAL = "ILLEGAL"
-	EOF     = "EOF"
+    ILLEGAL = "ILLEGAL"
+    EOF     = "EOF"
 
-	// Identifiers and literals
-	IDENT = "IDENT" // add, foo, x, y, ...
-	INT   = "INT"
+    // Identifiers and literals
+    IDENT  = "IDENT" // add, foo, x, y, ...
+    INT    = "INT"
+    STRING = "STRING"
 
-	// Operators
-	ASSIGN   = "="
-	PLUS     = "+"
-	MINUS    = "-"
-	BANG     = "!"
-	ASTERISK = "*"
-	SLASH    = "/"
+    // Operators
+    ASSIGN   = "="
+    PLUS     = "+"
+    MINUS    = "-"
+    BANG     = "!"
+    ASTERISK = "*"
+    SLASH    = "/"
 
-	LT = "<"
-	GT = ">"
+    LT = "<"
+    GT = ">"
 
-	EQ     = "=="
-	NOT_EQ = "!="
+    EQ     = "=="
+    NOT_EQ = "!="
 
-	// Delimiters
-	COMMA     = ","
-	SEMICOLON = ";"
+    // Delimiters
+    COMMA     = ","
+    SEMICOLON = ";"
 
-	LPAREN = "("
-	RPAREN = ")"
-	LBRACE = "{"
-	RBRACE = "}"
+    LPAREN = "("
+    RPAREN = ")"
+    LBRACE = "{"
+    RBRACE = "}"
 
-	// Keywords
-	FUNCTION = "FUNCTION"
-	LET      = "LET"
-	TRUE     = "TRUE"
-	FALSE    = "FALSE"
-	IF       = "IF"
-	ELSE     = "ELSE"
-	RETURN   = "RETURN"
+    // Keywords
+    FUNCTION = "FUNCTION"
+    LET      = "LET"
+    TRUE     = "TRUE"
+    FALSE    = "FALSE"
+    IF       = "IF"
+    ELSE     = "ELSE"
+    RETURN   = "RETURN"
 )
 
 var keywords = map[string]TokenType{
-	"fn":     FUNCTION,
-	"let":    LET,
-	"true":   TRUE,
-	"false":  FALSE,
-	"if":     IF,
-	"else":   ELSE,
-	"return": RETURN,
+    "fn":     FUNCTION,
+    "let":    LET,
+    "true":   TRUE,
+    "false":  FALSE,
+    "if":     IF,
+    "else":   ELSE,
+    "return": RETURN,
 }
 
 func LookupIdent(ident string) TokenType {
-	if tok, ok := keywords[ident]; ok {
-		return tok
-	}
-	return IDENT
+    if tok, ok := keywords[ident]; ok {
+        return tok
+    }
+    return IDENT
 }
