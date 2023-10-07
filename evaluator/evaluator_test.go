35478b7af720e898fa6ae26395af53fba29ef036
Tim Shilov <tim@shilov.dev>
2023-10-07 14:02:54 +0100

    feat(builtins): added support for arrays to 'len' function

--- evaluator/evaluator_test.go
+++ evaluator/evaluator_test.go
@@ -267,6 +267,8 @@ func TestBuiltinFunctions(t *testing.T) {
         {`len("")`, 0},
         {`len("four")`, 4},
         {`len("hello world")`, 11},
+        {`len([1, 2, 3])`, 3},
+        {`len([1, 2, 3, "4", "5"])`, 5},
         {`len(1)`, "argument to `len` not supported, got INTEGER"},
         {`len("one", "two")`, "wrong number of arguments. got=2, want=1"},
     }
