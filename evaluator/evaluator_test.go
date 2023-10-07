b3e90cc4715051e524e6929a79d98b6f6fd969ee
Tim Shilov <tim@shilov.dev>
2023-10-07 10:15:30 +0100

    feat(evaluator): added support for string concatenation

--- evaluator/evaluator_test.go
+++ evaluator/evaluator_test.go
@@ -143,6 +143,7 @@ func TestErrorHandling(t *testing.T) {
         {"if (10 > 1) { true + false; }", "unknown operator: BOOLEAN + BOOLEAN"},
         {"if(10 > 1) { if(10 > 1) { return true + false; } return 1; }", "unknown operator: BOOLEAN + BOOLEAN"},
         {"foobar", "identifier not found: foobar"},
+        {`"Hello" - "World"`, "unknown operator: STRING - STRING"},
     }
 
     for _, tt := range tests {
@@ -230,13 +231,27 @@ func TestClosures(t *testing.T) {
     testIntegerObject(t, testEval(input), 4)
 }
 
-func TestStringLiterals(t *testing.T){
+func TestStringLiterals(t *testing.T) {
     input := `"Hello World!"`
 
     evaluated := testEval(input)
-    str,ok := evaluated.(*object.String)
+    str, ok := evaluated.(*object.String)
     if !ok {
         t.Fatalf("object is not String. got=%T (%+v)", evaluated, evaluated)
+    }
+
+    if str.Value != "Hello World!" {
+        t.Errorf("String has wrong value. got=%q", str.Value)
+    }
+}
+
+func TestStringConcatenation(t *testing.T) {
+    input := `"Hello" + " " + "World!"`
+
+    evaluated := testEval(input)
+    str, ok := evaluated.(*object.String)
+    if !ok {
+        t.Fatalf("object is no String. got=%T (%+v)", evaluated, evaluated)
     }
 
     if str.Value != "Hello World!" {
